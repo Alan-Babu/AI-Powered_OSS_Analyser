2dfb2e1b427d42af95fd545c3c96587dcb55494d
Alan Babu <alan.wfo@gmail.com>
2025-11-02 21:37:33 +0530

    removing console.log

--- frontend/src/app/components/repository-scan/repository-scan.component.ts
+++ frontend/src/app/components/repository-scan/repository-scan.component.ts
@@ -81,7 +81,6 @@ export class RepositoryScanComponent implements OnInit, OnDestroy {
   get paginatedScanHistory(): RepositoryMetadata[] {
     const start = (this.currentPage - 1) * this.itemsPerPage;
     const end = start + this.itemsPerPage;
-    console.log(this.scanHistory);
     return this.scanHistory.slice(start, end);
   }
 
@@ -114,7 +113,6 @@ export class RepositoryScanComponent implements OnInit, OnDestroy {
         this.isLoadingHistory = false;
       }
     });
-    console.log(this.scanHistory);
     this.subscriptions.push(sub);
   }
 
